e7324781d5e313c0b4a977a1f8481a29fbab0ac3
Prabir Singh <91267692+prabircodes@users.noreply.github.com>
2021-12-17 20:53:46 +0530

    Add files via upload

--- Movie-Seat-Booking/js/script.js
+++ Movie-Seat-Booking/js/script.js
@@ -4,9 +4,22 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 let ticketPrice = +movieSelect.value;
+populateUi();
+//save selected movie index and price 
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem(selectedMovieIndex, movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
 
+//Update total count
 function updateSelectedCount () {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const seatIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatIndex));
+    
+
 
     const selectedSeatsCount = selectedSeats.length;
 
@@ -14,8 +27,28 @@ function updateSelectedCount () {
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// Get data from localstorage and populate UI
+function populateUi(){
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    if(selectedSeats != null && selectedSeats.length > 0){
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+
+    }
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if(selectedMovieIndex !== null){
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
+
+//Movie Select Event
 movieSelect.addEventListener ('change', (e) => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 
 })
@@ -30,3 +63,7 @@ container.addEventListener('click', (e) => {
         updateSelectedCount();
     }
 })
+
+// Initial count and total set
+
+updateSelectedCount();
